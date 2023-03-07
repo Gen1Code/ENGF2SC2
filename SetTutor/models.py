a3cfafaaa595ad0a0739a3f0c002269adb491124
Gen1Code <72555054+Gen1Code@users.noreply.github.com>
2023-03-07 17:44:40 +0000

    Revert "Revert "Read/Write Question File Methods""
    This reverts commit db2a39f5d5138880dac46a43ef576ee0bd6a7d44.

--- SetTutor/models.py
+++ SetTutor/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+def checkAnswer(Question,Answer):
+    with open("questions.txt","r") as f:
+        for line in f:
+            question = line.split(",")
+            if question[0] == Question:
+                return question[1] == Answer
+
+    print("Invalid QID")
+    raise KeyError
+
+def addQuestion(Question,Answer,Difficulty):
+    with open("questions.txt","a") as f:
+        f.write(Question+","+Answer+","+Difficulty)
