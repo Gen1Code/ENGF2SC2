4ca1f2ab174254901fb1aeea8739980005e18202
Gen1Code <72555054+Gen1Code@users.noreply.github.com>
2023-03-09 13:26:39 +0000

    Input Box in Questions

--- SetTutor/forms.py
+++ SetTutor/forms.py
@@ -1,12 +1,15 @@
 from django import forms
 
 DIFFICULTY_CHOICES = (
-    (1,"Easy"),
-    (2,"Meduim"),
-    (3,"Hard")
+    ("Easy","Easy"),
+    ("Meduim","Meduim"),
+    ("Hard","Hard")
 )
 
 class QuestionForm(forms.Form):
     Question = forms.CharField(label='Question', max_length=512)
     Answer = forms.CharField(label='Answer', max_length=128)
     Difficulty = forms.ChoiceField(label="Difficulty", choices=DIFFICULTY_CHOICES)
+
+class AnswerForm(forms.Form):
+    Answer = forms.CharField(label='Answer', max_length=128)
