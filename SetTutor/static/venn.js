331f2078d96bdefd9d43e1dd8a8134e5014880d8
Moiz-I <sky.moizi9@gmail.com>
2023-03-21 18:32:18 +0000

    labels and non-interactive venn

--- SetTutor/static/venn.js
+++ SetTutor/static/venn.js
@@ -1,57 +1,59 @@
 const venn = document.getElementById("venn");
 
-venn.addEventListener("click", function (event) {
-  // Remove active class from all paths
-  //   const paths = this.querySelectorAll('path');
-  //   paths.forEach(function(path) {
-  //     path.classList.remove('active');
-  //   });
-  // const paths = this.querySelectorAll("path");
-  // paths.forEach(function (path) {
-  //   path.classList.remove("highlight");
-  // });
-  // Add active class to matching elements
-  const svgId = event.target.getAttribute("id");
-  console.log(svgId);
-  const matchingElems = document.querySelectorAll("." + svgId);
+function canShadeVenn() {
+  venn.addEventListener("click", function (event) {
+    // Remove active class from all paths
+    //   const paths = this.querySelectorAll('path');
+    //   paths.forEach(function(path) {
+    //     path.classList.remove('active');
+    //   });
+    // const paths = this.querySelectorAll("path");
+    // paths.forEach(function (path) {
+    //   path.classList.remove("highlight");
+    // });
+    // Add active class to matching elements
+    const svgId = event.target.getAttribute("id");
+    console.log(svgId);
+    const matchingElems = document.querySelectorAll("." + svgId);
 
-  var classes = event.target.classList;
-  var classesList = Array.from([...classes]);
-  if (classesList.includes("active")) {
-    event.target.classList.remove("active");
-  } else {
-    event.target.classList.add("active");
-  }
+    var classes = event.target.classList;
+    var classesList = Array.from([...classes]);
+    if (classesList.includes("active")) {
+      event.target.classList.remove("active");
+    } else {
+      event.target.classList.add("active");
+    }
 
-  // matchingElems.forEach(function (elem) {
-  //   if (elem.className == "active") {
-  //     elem.classList.remove("active");
-  //   } else {
-  //     elem.classList.add("active");
-  //   }
-  // });
+    // matchingElems.forEach(function (elem) {
+    //   if (elem.className == "active") {
+    //     elem.classList.remove("active");
+    //   } else {
+    //     elem.classList.add("active");
+    //   }
+    // });
 
-  // Add active class to clicked element
-});
-
-venn.addEventListener("mouseover", function (event) {
-  const paths = this.querySelectorAll("path");
-  paths.forEach(function (path) {
-    path.classList.remove("highlight");
+    // Add active class to clicked element
   });
+}
 
-  // Add active class to matching elements
-  const svgId = event.target.getAttribute("id");
-  const matchingElems = document.querySelectorAll("." + svgId);
+// venn.addEventListener("mouseover", function (event) {
+//   const paths = this.querySelectorAll("path");
+//   paths.forEach(function (path) {
+//     path.classList.remove("highlight");
+//   });
 
-  var classes = event.target.classList;
-  var classesList = Array.from([...classes]);
-  if (classesList.includes("highlight")) {
-    event.target.classList.remove("highlight");
-  } else {
-    event.target.classList.add("highlight");
-  }
-});
+//   // Add active class to matching elements
+//   const svgId = event.target.getAttribute("id");
+//   const matchingElems = document.querySelectorAll("." + svgId);
+
+//   var classes = event.target.classList;
+//   var classesList = Array.from([...classes]);
+//   if (classesList.includes("highlight")) {
+//     event.target.classList.remove("highlight");
+//   } else {
+//     event.target.classList.add("highlight");
+//   }
+// });
 
 function getRegions() {
   const active = document.getElementsByClassName("active");
@@ -77,8 +79,12 @@ function shadeRegions(set) {
     path.classList.remove("active");
   });
 
-  const regionsList = set.replace("{","").replace("}","").replaceAll("&#x27;","").split(",");
-  
+  const regionsList = set
+    .replace("{", "")
+    .replace("}", "")
+    .replaceAll("&#x27;", "")
+    .split(",");
+
   for (i in regionsList) {
     const matchingElems = document.getElementById(regionsList[i]);
     matchingElems.classList.add("active");
